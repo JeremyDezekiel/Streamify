69b03f4ab4ebd59d30ebdbd941561eff650c422e
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-26 13:43:28 +0700

    add demo content

--- src/App.jsx
+++ src/App.jsx
@@ -8,55 +8,82 @@ function App() {
 
   return (
     <>
-      <section>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Movie.com</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><hr className="dropdown-divider"/></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <main>
+        <section>
+          <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">Movie.com</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">TV Shows</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Movie</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">New & Popular</a>
+                  </li><li className="nav-item">
+                    <a className="nav-link" href="#">My List</a>
+                  </li><li className="nav-item">
+                    <a className="nav-link" href="#">Browse by Languages</a>
+                  </li>
+                </ul>
+                <form className="d-flex" role="search">
+                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                   <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
-              <ul className="navbar-nav mb-2 mb-lg-0">
-                <li className="nav-item dropdown">
-                  <span className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img className='profile-icon' src='https://occ-0-58-64.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABV7Sri1-D7qEbxbugWRmCLNbtHHf8ZnmqzUv-mNSrHyzV8wW79qYqLFYhCXARsp6d87_p46hVjDnj1qCMpmsaugwMo9nLr8.png?r=ddf' alt='profileIcon' />
-                  </span>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><hr class="dropdown-divider" /></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
+                </form>
+                <ul className="navbar-nav mb-2 mb-lg-0">
+                  <li className="nav-item dropdown">
+                    <span className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img className='profile-icon' src='https://occ-0-58-64.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABV7Sri1-D7qEbxbugWRmCLNbtHHf8ZnmqzUv-mNSrHyzV8wW79qYqLFYhCXARsp6d87_p46hVjDnj1qCMpmsaugwMo9nLr8.png?r=ddf' alt='profileIcon' />
+                    </span>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                      <li><hr class="dropdown-divider" /></li>
+                      <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="position-relative">
+            <div>
+              <div className='position-absolute top-50 start-0 translate-middle-y ms-5'>
+                <div>
+                  <img alt='' className='' src='https://occ-0-58-64.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUCBN072AyO2JluEcf6t1SvtMBsO7yQ_rhU9fuMYDzr9xVtnUmOCNzuPwPZhfxz6pBrQdzFm-cg1V5Wb0o3VzZ8jWv6gQaVf_EK4dz3T_4awSHqRsoIG1ZQrTUFFO_Rdueq_BfCaLdiTp41kjGIi7pakraiptG8pw5K5o29mWJ6Wif0Gad8nJA.webp?r=15b' title='' />
+                </div>
+                <div>
+                  <p>
+                    Ex-insiders from the world's biggest brands share the manipulative tricks they use to keep customers consuming — at a devastating cost to our lives.
+                  </p>
+                </div>
+              </div>
+              <div className='position-absolute top-50 end-0 translate-middle-y me-5'>
+                <div>
+
+                </div>
+              </div>
+              <img className='w-100' src='https://occ-0-58-64.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABV0-RxSQcYC25cCe4B6hJQlo5_8u1J8ZQA-aPv_CwRWjh3YdF2sGHWYLr47NZRxgYRAqnVF6fpBM_hGJ9KRL29L35yt6axU86iNd.webp?r=f78' alt='' />
             </div>
           </div>
-        </nav>
-      </section>
+        </section>
+        <section>
+          <div>
+            
+          </div>
+        </section>
+      </main>
     </>
   )
 }
