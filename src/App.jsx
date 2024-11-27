9e2a99190b3ee61290ecb6a255b645cfa04188ef
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-27 12:05:35 +0700

    redesign

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <main>
+      {/* <main>
         <section>
           <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
@@ -44,12 +44,12 @@ function App() {
                     <span className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                       <img className='profile-icon' src='https://occ-0-58-64.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABV7Sri1-D7qEbxbugWRmCLNbtHHf8ZnmqzUv-mNSrHyzV8wW79qYqLFYhCXARsp6d87_p46hVjDnj1qCMpmsaugwMo9nLr8.png?r=ddf' alt='profileIcon' />
                     </span>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                      <li><hr class="dropdown-divider" /></li>
-                      <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Another action</a></li>
+                      <li><a className="dropdown-item" href="#">Something else here</a></li>
+                      <li><hr className="dropdown-divider" /></li>
+                      <li><a className="dropdown-item" href="#">Separated link</a></li>
                     </ul>
                   </li>
                 </ul>
@@ -66,20 +66,20 @@ function App() {
                   <p className='fs-4 text-white'>
                     Ex-insiders from the world's biggest brands share the manipulative tricks they use to keep customers consuming — at a devastating cost to our lives.
                   </p>
-                  <div class="">
+                  <div className="">
                     <div className='row'>
                       <div className='col'>
-                        <button type="button" class="btn btn-secondary btn-lg">Play</button>
+                        <button type="button" className="btn btn-secondary btn-lg">Play</button>
                       </div>
                       <div className='col'>
-                        <button type="button" class="btn btn-secondary btn-lg">More Info</button>
+                        <button type="button" className="btn btn-secondary btn-lg">More Info</button>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className='col-lg-4'>
                   <div className='position-absolute bottom-50 end-0 translate-middle-y me-5'>
-                    <button type="button" class="btn btn-secondary btn-lg">More Info</button>
+                    <button type="button" className="btn btn-secondary btn-lg">Restart</button>
                     <span className='fs-1 text-white'>
                       13+
                     </span>
@@ -99,22 +99,22 @@ function App() {
                   </h2>
                   <div className='d-flex flex-row'>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXMQiKl7WK7Ye0nsPacAmUPseRS3k_RCn71_cwn5oYSuue9qJ5MBPUWSwyXIqxMLe6MKxs_zNygXgTN2iDkMGd9W8mc586yVLnKUmW7b40OsSbbUlVRiLht1N60JvEzY68F9ZPlL3HITeVMmpzHTUcs46lIQygHJMG5d7WO-VNTeX-rUtun0p6MByJc9qgWw6H4Fd8RGLG_B_tTn7coEZKkVyLAiNT21fvZnSKOs5TxQ_IyPp9ZMLGMJuJYzoE8Zv_1PsUKa06td6Qe9fI0t-mYnKqnlKO5VGXkKBY4u_aezyBXbryk4fgAL_TrE0aIWcrn7NXRfBYdieyY8nKpftbc.webp?r=67a" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXMQiKl7WK7Ye0nsPacAmUPseRS3k_RCn71_cwn5oYSuue9qJ5MBPUWSwyXIqxMLe6MKxs_zNygXgTN2iDkMGd9W8mc586yVLnKUmW7b40OsSbbUlVRiLht1N60JvEzY68F9ZPlL3HITeVMmpzHTUcs46lIQygHJMG5d7WO-VNTeX-rUtun0p6MByJc9qgWw6H4Fd8RGLG_B_tTn7coEZKkVyLAiNT21fvZnSKOs5TxQ_IyPp9ZMLGMJuJYzoE8Zv_1PsUKa06td6Qe9fI0t-mYnKqnlKO5VGXkKBY4u_aezyBXbryk4fgAL_TrE0aIWcrn7NXRfBYdieyY8nKpftbc.webp?r=67a" classNameName="card-img-top" alt="..."/>
                     </div>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABb3z_J3XGQztP0TN8korj7rJOSUzLg_TBoblj_5cVaN6MGTWpXfb2JVYzk3ssu1JpONDShhsk08mYz_iK9jFywWFucPHOlMB45DyN4uTb50ltZQHUE_RPhaSi5h0U0jP84_P.jpg?r=872" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABb3z_J3XGQztP0TN8korj7rJOSUzLg_TBoblj_5cVaN6MGTWpXfb2JVYzk3ssu1JpONDShhsk08mYz_iK9jFywWFucPHOlMB45DyN4uTb50ltZQHUE_RPhaSi5h0U0jP84_P.jpg?r=872" classNameName="card-img-top" alt="..."/>
                     </div>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABVP12YvFY34JSp9OjsOxLrxVB-AG6_FyFb2gVCpijKfzJxTYeNUrTgh80AcELW2wxLZEyuhLAhDYTfDIbaTFj7Rf5XzT1l2LTRe0oEh45vhhTTEmZ9DWF1IwjhVIOaRMnqSEpojU2zbIPZzRpTh-rag9jNbG_WXjHxyvlpDfsO0YJecu3ly8ukoNilcsVgZLgEocBWJiwhiOmrNvBNtL4MJJLn9v6qdo3DyCEn_8bZRSL43OBVJ1zywM_s_WLUqwc3daF85WZcBE35O4_NGLgQTF3wh1Arj5Ofsly3noDcgmoIAXtKWnEo_CbrjNK027NbD7OwKJuxOYd8G6tg4ziTE.webp?r=f87" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABVP12YvFY34JSp9OjsOxLrxVB-AG6_FyFb2gVCpijKfzJxTYeNUrTgh80AcELW2wxLZEyuhLAhDYTfDIbaTFj7Rf5XzT1l2LTRe0oEh45vhhTTEmZ9DWF1IwjhVIOaRMnqSEpojU2zbIPZzRpTh-rag9jNbG_WXjHxyvlpDfsO0YJecu3ly8ukoNilcsVgZLgEocBWJiwhiOmrNvBNtL4MJJLn9v6qdo3DyCEn_8bZRSL43OBVJ1zywM_s_WLUqwc3daF85WZcBE35O4_NGLgQTF3wh1Arj5Ofsly3noDcgmoIAXtKWnEo_CbrjNK027NbD7OwKJuxOYd8G6tg4ziTE.webp?r=f87" classNameName="card-img-top" alt="..."/>
                     </div>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABeCyEX58PwPpegcbW3T_J5wR6Ogxf1dxCAWM62_rDigZu6ee3qyxoNgFMS9H8RLkCQ-JUdxk9Wx6Xabo06D6IguzBH692whymFnDXbPKtD6m8avkPFk0lHYea3YJ0GVyB4c-k1Gxp6REvPOBv0EZtjivACXuKa74VekEaJ7P0yAbtveWdw63hwMqActYy8oGossKQpzAPtdl5MWkeFsI_YifPmSQ3MC5q7EjU5emfLGDSNwQAus3cJGDD3Ix1ydhLJFXguugfFJfd2ED2QZ0mxSAwIEwEQDAeQoNxr3DpsQmWxiSQPXuKr5XxJnvnKvG3S7imX-GlFwOGzCAxVgIe5k.webp?r=83b" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABeCyEX58PwPpegcbW3T_J5wR6Ogxf1dxCAWM62_rDigZu6ee3qyxoNgFMS9H8RLkCQ-JUdxk9Wx6Xabo06D6IguzBH692whymFnDXbPKtD6m8avkPFk0lHYea3YJ0GVyB4c-k1Gxp6REvPOBv0EZtjivACXuKa74VekEaJ7P0yAbtveWdw63hwMqActYy8oGossKQpzAPtdl5MWkeFsI_YifPmSQ3MC5q7EjU5emfLGDSNwQAus3cJGDD3Ix1ydhLJFXguugfFJfd2ED2QZ0mxSAwIEwEQDAeQoNxr3DpsQmWxiSQPXuKr5XxJnvnKvG3S7imX-GlFwOGzCAxVgIe5k.webp?r=83b" classNameName="card-img-top" alt="..."/>
                     </div>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABeoa4BFwDJiCUNTZdCSQG4yab0MDEwYuZ4up56k5Pq-x_G2mIBa3L_iRKLlUzk9cPHZ-Zzj0j7TwYfnyFYp82AleLb4_TX52zss.webp?r=dbe" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABeoa4BFwDJiCUNTZdCSQG4yab0MDEwYuZ4up56k5Pq-x_G2mIBa3L_iRKLlUzk9cPHZ-Zzj0j7TwYfnyFYp82AleLb4_TX52zss.webp?r=dbe" classNameName="card-img-top" alt="..."/>
                     </div>
                     <div className='card' style={{width: '20rem',}}>
-                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZ_wyuaKDX2rC8U6CONyA1ZeHvlF8tcl3bTw7rcp4dnY9ziAEwIy_Gb6-cUO7khOgUB9sQoOr5YZcDqZ7JL0UZD2ImpFysUikCo.webp?r=01d" className="card-img-top" alt="..."/>
+                      <img src="https://occ-0-58-64.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZ_wyuaKDX2rC8U6CONyA1ZeHvlF8tcl3bTw7rcp4dnY9ziAEwIy_Gb6-cUO7khOgUB9sQoOr5YZcDqZ7JL0UZD2ImpFysUikCo.webp?r=01d" classNameName="card-img-top" alt="..."/>
                     </div>
                   </div>
                 </div>
@@ -122,15 +122,48 @@ function App() {
             </div>
           </div>
         </section>
+      </main> */}
 
-
-
-        <section>
-          <div>
-
-          </div>
-        </section>
-      </main>
+      <section>
+        <div className='col-12'>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"/></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div className='col-12'>
+          <div className=''></div>
+        </div>
+      </section>
     </>
   )
 }
