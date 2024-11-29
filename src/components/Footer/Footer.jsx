5fbf82a76bf5cd3edb3fa03f680c456804845222
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-29 16:51:03 +0700

    add footer and animation Explore All

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from "react"
 import './Footer.css'
+import facebookIcon from '../../assets/facebookIcon.png'
+import instagramIcon from '../../assets/instagramIcon.png'
+import twitterIcon from '../../assets/twitterIcon.png'
+import youtubeIcon from '../../assets/youtubeIcon.png'
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className="footer">
+            <div className="footerIcon">
+                <img src={facebookIcon} alt=""/>
+                <img src={instagramIcon} alt=""/>
+                <img src={twitterIcon} alt=""/>
+                <img src={youtubeIcon} alt=""/>
+            </div>
+            <ul>
+                <li>Audio Description</li>
+                <li>Help Center</li>
+                <li>Gift Cards</li>
+                <li>Media Center</li>
+                <li>Investor Relations</li>
+                <li>Jobs</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Legal Notices</li>
+                <li>Cookie Preferences</li>
+                <li>Corporate Information</li>
+                <li>Contact Us</li>
+            </ul>
+            <div className="button">
+                <button>Service Code</button>
+            </div>
+            <span className="copyright">
+                © 1997-2024 Netflix, Inc.
+            </span>
         </div>
     )
 }
